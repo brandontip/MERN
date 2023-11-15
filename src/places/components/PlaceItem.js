4e71aa4a5cd3ac89740b6637b3bc3291f654a995
Brandon Tippings <tippingsbrandon@gmail.com>
2023-11-15 00:01:18 -0600

    Added maps

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -3,6 +3,7 @@ import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import {useState} from "react";
 import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
 
 function PlaceItem(props){
     const [showMap, setShowMap] = useState(false);
@@ -20,7 +21,7 @@ function PlaceItem(props){
             footer={<Button onClick={closeMapHandler}>Close</Button>}
         >
             <div className="map-container">
-                <h2>THE MAP</h2>
+                <Map center={props.coordinates} zoom={16}/>
             </div>
         </Modal>
         <li className="place-item">
---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,54 @@
+
+import "./Map.css"
+// import {useRef, useEffect} from "react";
+import { GoogleMap,  useLoadScript } from "@react-google-maps/api";
+
+function Map(props){
+    // let map;
+    // const mapRef = useRef();
+    const {center, zoom} = props;
+
+    // async function initMap() {
+    //     const { Map } = await google.maps.importLibrary("maps");
+    //     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+    //
+    //
+    //     map = new Map(mapRef.current, {
+    //         center: center,
+    //         zoom: zoom,
+    //     });
+    //
+    //     const marker = new AdvancedMarkerElement({
+    //         map: map,
+    //         position: center
+    //     });
+    // }
+    //
+    //
+    //
+    // useEffect(() => {
+    //
+    //
+    // }, [center, zoom]);
+    const { isLoaded } = useLoadScript({
+        id: "google-map-script",
+        googleMapsApiKey: "AIzaSyAd6WBYUaD5U0IgrQR-nYrwQhUyxluxYEM",
+    });
+
+
+
+    return <div className="App">
+        {!isLoaded ? (
+            <h1>Loading...</h1>
+        ) : (
+            <GoogleMap
+                mapContainerClassName="map-container"
+                center={center}
+                zoom={zoom}
+            />
+        )}
+    </div>
+    // return <div ref={mapRef} className={`map ${props.className}`} style={props.style}>{props.children}</div>
+}
+
+export default Map;
